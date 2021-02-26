8eb9b0c1293fad1e93ff9996d81abf0ccbc81943
JuliaMartins97 <martinss.s.julia@gmail.com>
2021-02-26 18:38:12 -0300

    ex017

--- aula 14ex/ex 016/script.js
+++ aula 14ex/ex 016/script.js
@@ -1,11 +1,32 @@
-function contar(){
-    var ini = document.getElementById('txtini')
-    var fim = document.getElementById('txtf')
-    var passo = document.getElementById('txtp')
-    var res = document.getElementById('res')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    if (i.value.length == 0 || f.value.length == 0 || p.value.length == 0) {
-        window.alert('[ERRO]Faltam dados!')
-    } //else {
-        //alert('Tudo certo!')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+        //window.alert('[ERRO] Faltam dados!')
+    } else {
+     res.innerHTML ='Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number (passo.value)
+        if (p <=0){
+            window.alert('Passo inválido! Considerando passo 1')
+            p=1
+        }
+    
+        if (i < f){ 
+            //Contagem crescente 
+        for (let c = i; c <=f; c +=p ) {
+            res.innerHTML += `${c} \u{27A1}`
+        }
+    } else { //Contagem regressiva 
+        for(let c = i; c >= f; c -= p) {
+            res.innerHTML += `${c} \u{27A1}`
+        }
+    }
+    res.innerHTML += `\u{1F3C1}`
+    }
     }
