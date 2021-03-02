3dd09b68271a1a6226eb4873aaffcbec37936de7
JuliaMartins97 <martinss.s.julia@gmail.com>
2021-03-02 17:53:24 -0300

    atualização aulas

---
+++ aula 16/função01.js
@@ -0,0 +1,9 @@
+function parimpar(n){
+    if (n%2 == 0){ //lê -se: Se o número dividido por 2 tiver resto igual a 0 
+        return 'Par!'
+    } else{
+        return 'Ímpar!'
+    }
+}
+
+console.log(parimpar(4))
